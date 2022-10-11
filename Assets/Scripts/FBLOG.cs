9d6ff3ff0d6868eda9d8ce4ca08361dfa637a8b2
snwork <snwork89@gmail.com>
2022-10-11 09:12:05 +0530

    Facebook Script

---
+++ Assets/Scripts/FBLOG.cs
@@ -0,0 +1,43 @@
+using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBLOG : MonoBehaviour
+{
+    void Awake()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            //Handle FB.Init
+            FB.Init(() => {
+                FB.ActivateApp();
+            });
+        }
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // Check the pauseStatus to see if we are in the foreground
+        // or background
+        if (!pauseStatus)
+        {
+            //app resume
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+            else
+            {
+                //Handle FB.Init
+                FB.Init(() => {
+                    FB.ActivateApp();
+                });
+            }
+        }
+    }
+}
